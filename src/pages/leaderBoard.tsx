83fb2faf728ad071d8f0a594a8a7d6950088b08d
sportsbookdev510 <knightsdex>
2026-01-23 04:16:46 -0500

    .

--- src/pages/leaderBoard.tsx
+++ src/pages/leaderBoard.tsx
@@ -47,87 +47,96 @@ const LeaderboardTable = () => {
         if (error) {
             alert(error.message)
         }
-    }, []);
+    }, [error]);
+
+    const stakersCount = data?.stakers?.length || 0;
 
     return (
         <>
             {
                 loading ? <Spinner size='12' color='white' /> : (
                     <>
-                        <div className="flex flex-col md:flex-row lg:justify-center justify-around gap-6 items-center w-full pt-10 pb-10">
-                            <LeaderboardCard data={data?.stakers[0]} ranking={1} />
-                            <LeaderboardCard data={data?.stakers[1]} ranking={2} />
-                            <LeaderboardCard data={data?.stakers[2]} ranking={3} />
-                        </div>
-                        <div className="w-full lg:flex hidden flex-col items-center relative bg-card-bg rounded-3xl overflow-hidden md:px-8 px-4 md:py-6 py-3">
-                            {/* Table Header */}
-                            <div className="w-full rounded-t-3xl flex justify-between items-center md:px-6 px-1 py-4 text-primary md:text-lg text-sm font-bold sticky top-0 z-10">
-                                <span className="md:w-1/12 w-1/12">#</span>
-                                <span className="md:w-4/12 w-3/12">User</span>
-                                <span className="md:w-2/12 w-3/12">Joined</span>
-                                <span className="md:w-2/12 w-2/12">Status</span>
-                                <span className="md:w-2/12 w-2/12">Staked</span>
-                                <span className="md:w-1/12 w-1/12"></span>
+                        {stakersCount > 0 && (
+                            <div className="flex flex-col md:flex-row lg:justify-center justify-around gap-6 items-center w-full pt-10 pb-10">
+                                {stakersCount >= 1 && <LeaderboardCard data={data?.stakers[0]} ranking={1} />}
+                                {stakersCount >= 2 && <LeaderboardCard data={data?.stakers[1]} ranking={2} />}
+                                {stakersCount >= 3 && <LeaderboardCard data={data?.stakers[2]} ranking={3} />}
                             </div>
+                        )}
+                        
+                        {stakersCount >= 3 && (
+                            <>
+                                <div className="w-full lg:flex hidden flex-col items-center relative bg-card-bg rounded-3xl overflow-hidden md:px-8 px-4 md:py-6 py-3">
+                                    {/* Table Header */}
+                                    <div className="w-full rounded-t-3xl flex justify-between items-center md:px-6 px-1 py-4 text-primary md:text-lg text-sm font-bold sticky top-0 z-10">
+                                        <span className="md:w-1/12 w-1/12">#</span>
+                                        <span className="md:w-4/12 w-3/12">User</span>
+                                        <span className="md:w-2/12 w-3/12">Joined</span>
+                                        <span className="md:w-2/12 w-2/12">Status</span>
+                                        <span className="md:w-2/12 w-2/12">Staked</span>
+                                        <span className="md:w-1/12 w-1/12"></span>
+                                    </div>
 
-                            {/* Scrollable Rows */}
-                            <div className="w-full overflow-y-auto">
-                                {data?.stakers.map((row, index) => (
-                                    <div
-                                        key={index}
-                                        className={`w-full flex justify-between items-center md:px-6 px-1 py-4 text-light text-sm md:text-lg backdrop-blur border-t border-light-border`}
-                                    >
-                                        <span className="md:w-1/12 w-1/12">{index + 1}</span>
-                                        <div className="md:w-4/12 w-3/12 flex items-center md:space-x-4 space-x-2">
-                                            <img
-                                                src={index === 0 ? Gold_avatar : index === 1 ? Silver_avatar : index === 2 ? Bronze_avatar : Blue_avatar}
-                                                alt="Avatar"
-                                                className="md:w-10 md:h-10 w-8 h-8"
-                                            />
-                                            <span className="hidden md:flex">{(row.address).slice(0, 7) + '...' + (row.address).slice(-5)}</span>
-                                            <span className="md:hidden">{(row.address).slice(0, 7)}...</span>
-                                        </div>
-                                        <span className="md:w-2/12 w-3/12">{new Date(Number(row.createdAt) * 1000).toLocaleDateString()}</span>
-                                        <span className="md:w-2/12 w-2/12">Legend</span>
-                                        <span className="md:w-2/12 w-2/12">
-                                            {shortNumber(Math.floor?.(Number(row.currentlyStaked) / 10 ** 18))}
-                                        </span>
-                                        <span className="md:w-1/12 w-1/12">WMTx</span>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                        <div className="w-full lg:hidden flex flex-col relative bg-card-bg rounded-3xl overflow-hidden md:px-8 px-4 md:py-6 py-3">
-                            <div className="space-y-4 w-full">
-                                {data?.stakers.map((row, index) => (
-                                    <div className="flex flex-row justify-between border-b border-b-light-border last:border-b-0 pb-4" key={index}>
-                                        <div className="flex flex-col space-y-4 justify-center text-primary">
-                                            <span className="">#</span>
-                                            <span className="">User</span>
-                                            <span className="">Joined</span>
-                                            <span className="">Status</span>
-                                            <span className="">Staked</span>
-                                        </div>
-                                        <div className="flex flex-col space-y-4 justify-center items-end text-light">
-                                            <span className="">{index + 1}</span>
-                                            <div className="flex items-center md:space-x-4 space-x-2">
-                                                <img
-                                                    src={index === 0 ? Gold_avatar : index === 1 ? Silver_avatar : index === 2 ? Bronze_avatar : Blue_avatar}
-                                                    alt="Avatar"
-                                                    className="w-8 h-8"
-                                                />
-                                                <span className="flex">{(row.address).slice(0, 7) + '...' + (row.address).slice(-5)}</span>
+                                    {/* Scrollable Rows */}
+                                    <div className="w-full overflow-y-auto">
+                                        {data?.stakers.map((row, index) => (
+                                            <div
+                                                key={index}
+                                                className={`w-full flex justify-between items-center md:px-6 px-1 py-4 text-light text-sm md:text-lg backdrop-blur border-t border-light-border`}
+                                            >
+                                                <span className="md:w-1/12 w-1/12">{index + 1}</span>
+                                                <div className="md:w-4/12 w-3/12 flex items-center md:space-x-4 space-x-2">
+                                                    <img
+                                                        src={index === 0 ? Gold_avatar : index === 1 ? Silver_avatar : index === 2 ? Bronze_avatar : Blue_avatar}
+                                                        alt="Avatar"
+                                                        className="md:w-10 md:h-10 w-8 h-8"
+                                                    />
+                                                    <span className="hidden md:flex">{(row.address).slice(0, 7) + '...' + (row.address).slice(-5)}</span>
+                                                    <span className="md:hidden">{(row.address).slice(0, 7)}...</span>
+                                                </div>
+                                                <span className="md:w-2/12 w-3/12">{new Date(Number(row.createdAt) * 1000).toLocaleDateString()}</span>
+                                                <span className="md:w-2/12 w-2/12">Legend</span>
+                                                <span className="md:w-2/12 w-2/12">
+                                                    {shortNumber(Math.floor?.(Number(row.currentlyStaked) / 10 ** 18))}
+                                                </span>
+                                                <span className="md:w-1/12 w-1/12">WMTx</span>
                                             </div>
-                                            <span className="">{new Date(Number(row.createdAt) * 1000).toLocaleDateString()}</span>
-                                            <span className="">Legend</span>
-                                            <span className="">
-                                                {shortNumber(Math.floor?.(Number(row.currentlyStaked) / 10 ** 18)) + ' WNTx'}
-                                            </span>
-                                        </div>
+                                        ))}
                                     </div>
-                                ))}
-                            </div>
-                        </div>
+                                </div>
+                                <div className="w-full lg:hidden flex flex-col relative bg-card-bg rounded-3xl overflow-hidden md:px-8 px-4 md:py-6 py-3">
+                                    <div className="space-y-4 w-full">
+                                        {data?.stakers.map((row, index) => (
+                                            <div className="flex flex-row justify-between border-b border-b-light-border last:border-b-0 pb-4" key={index}>
+                                                <div className="flex flex-col space-y-4 justify-center text-primary">
+                                                    <span className="">#</span>
+                                                    <span className="">User</span>
+                                                    <span className="">Joined</span>
+                                                    <span className="">Status</span>
+                                                    <span className="">Staked</span>
+                                                </div>
+                                                <div className="flex flex-col space-y-4 justify-center items-end text-light">
+                                                    <span className="">{index + 1}</span>
+                                                    <div className="flex items-center md:space-x-4 space-x-2">
+                                                        <img
+                                                            src={index === 0 ? Gold_avatar : index === 1 ? Silver_avatar : index === 2 ? Bronze_avatar : Blue_avatar}
+                                                            alt="Avatar"
+                                                            className="w-8 h-8"
+                                                        />
+                                                        <span className="flex">{(row.address).slice(0, 7) + '...' + (row.address).slice(-5)}</span>
+                                                    </div>
+                                                    <span className="">{new Date(Number(row.createdAt) * 1000).toLocaleDateString()}</span>
+                                                    <span className="">Legend</span>
+                                                    <span className="">
+                                                        {shortNumber(Math.floor?.(Number(row.currentlyStaked) / 10 ** 18)) + ' WNTx'}
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                            </>
+                        )}
                     </>
                 )
             }
@@ -137,6 +146,13 @@ const LeaderboardTable = () => {
 };
 
 const LeaderBoard: React.FC = () => {
+    const { loading, data } = useQuery<GetTopStakersData>(GET_TOP_STAKERS, {
+        client,
+    });
+
+    const stakersCount = data?.stakers?.length || 0;
+    const title = stakersCount === 0 ? "Let's Stake" : "Top Stakers";
+
     return (
         <div className="bg-primary-bg rounded-b-[60px] relative z-[10]">
             <div className="w-full 2xl:w-[1280px] 2xl:mx-auto md:px-8 2xl:px-0 px-4 pb-[150px]">
@@ -145,8 +161,8 @@ const LeaderBoard: React.FC = () => {
                     {/* Content */}
                     <div className="relative flex flex-col items-start justify-start min-h-lvh text-white z-10 pt-20 gap-3">
                         {/* <div className="md:text-lg text-lg font-bold border border-white text-white rounded-tr-3xl rounded-bl-3xl w-max px-6 py-1 mt-10 mb:mt-20">Leaderboard</div> */}
-                        <div className="text-6xl 2xl:text-9xl xl:text-8xl lg:text-7xl md:text-5xl mt-5 mb:mt-20 lg:w-[95%] w-full text-primary font-semibold text-align: center;">Top Stakers</div>
-                        <LeaderboardTable />
+                        <div className="text-6xl 2xl:text-9xl xl:text-8xl lg:text-7xl md:text-5xl mt-5 mb:mt-20 lg:w-[95%] w-full text-primary font-semibold text-align: center;">{title}</div>
+                        {!loading && stakersCount > 0 && <LeaderboardTable />}
                     </div>
                 </div>
             </div>
